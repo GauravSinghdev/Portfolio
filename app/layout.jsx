fe6fe58a7cc785f0332597dee3ca05c1b504ad60
GauravSinghdev <gauravkalakoti19@gmail.com>
2024-08-06 03:28:27 +0530

    adding logo

--- app/layout.jsx
+++ app/layout.jsx
@@ -18,6 +18,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <link rel="icon" type="image/svg+xml" href="https://png.pngtree.com/png-vector/20190223/ourmid/pngtree-vector-portfolio-icon-png-image_695874.jpg" />
       <title>Portfolio - @codewithkara</title>
       <body className={jetBrainsMono.variable}>
         <Header/>
